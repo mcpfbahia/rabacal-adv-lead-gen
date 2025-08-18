d8a4c602c94910872bc9d853882ff04636504103
José de Sena Moreira Santos <mcpfba03@gmail.com>
2025-08-18 06:48:56 -0300

    remoção botão scroll indicador sem função da HeroSection

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -167,13 +167,6 @@ const HeroSection = () => {
           </div>
         </div>
       </div>
-      
-      {/* Scroll Indicator */}
-      <div className="absolute bottom-6 left-1/2 transform -translate-x-1/2 animate-bounce">
-        <div className="w-6 h-10 border-2 border-white/50 rounded-full flex justify-center backdrop-blur-sm">
-          <div className="w-1 h-3 bg-white rounded-full mt-2 animate-pulse"></div>
-        </div>
-      </div>
     </section>
   );
 };
